bb5025061bc88c48bc07ff65f18e6bdcb965ee96
obando_2906 <galloesteban57@gmail.com>
2025-10-15 00:21:44 -0500

    Deploy Demo

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-from config import Config
+from config import ProductionConfig, DevelopmentConfig
 from app.models import db
 from rutas import register_blueprints
-
+import os
 
 def create_app():
     app = Flask(__name__)
-    
-    app.config.from_object(Config)
 
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+    if os.environ.get("RENDER") == "true": 
+        app.config.from_object(ProductionConfig)
+        cors_origins = ["https://tu-frontend.netlify.app"]  
+    else:
+        app.config.from_object(DevelopmentConfig)
+        cors_origins = ["http://localhost:5173"] 
+
+    CORS(app, resources={r"/*": {"origins": cors_origins}}, supports_credentials=True)
 
     db.init_app(app)
 
