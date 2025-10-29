a0a6f1bdf14cc56f8147d47dee54d977a1071fb6
obando_2906 <galloesteban57@gmail.com>
2025-10-29 16:41:42 -0500

    Pruebas Localhost 1.0.0

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,26 @@ from flask_cors import CORS
 from config import ProductionConfig, DevelopmentConfig
 from app.models import db
 from rutas import register_blueprints
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
-    if os.environ.get("RENDER") == "true": 
+    environment = os.getenv("ENV", "local")
+
+    if environment == "production":
         app.config.from_object(ProductionConfig)
-        cors_origins = ["https://system-control.netlify.app"]  
+        cors_origins = ["https://system-control.netlify.app"]
     else:
         app.config.from_object(DevelopmentConfig)
-        cors_origins = ["http://localhost:5173"] 
+        cors_origins = ["http://localhost:5432"]
 
     CORS(app, resources={r"/*": {"origins": cors_origins}}, supports_credentials=True)
 
     db.init_app(app)
-
     register_blueprints(app)
 
     return app
